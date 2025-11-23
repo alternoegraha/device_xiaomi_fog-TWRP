fe84e72246b20453c7d41bbeea5e4fbc7271205e
Alterina <noegrahachan@gmail.com>
2025-11-23 06:00:33 +0000

    switch to new kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
 # Clone kernel tree
-git clone --depth=1 https://github.com/alternoegraha/wwy_kernel_xiaomi_fog_rebase kernel/xiaomi/fog
+git clone --depth=1 -b ye https://github.com/alternoegraha/kernel_xiaomi_sm6225 kernel/xiaomi/fog
